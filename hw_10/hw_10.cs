bd18ac2b721354a6f7d8c335050de263a99a9eec
Netkachev <mnetckachev@yandex.ru>
2023-07-02 16:27:54 +0300

    Задача 10

---
+++ hw_10/hw_10.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+using System;
+
+Clear();
+Write("Введите трехзначное число ");
+int a = int.Parse(ReadLine());
+
+int result = a % 100 / 10;
+WriteLine($"{a} -> {result}");
