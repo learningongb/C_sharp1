3217d5a7137a3bfed7a43a7b4d897f4920e69824
Netkachev <mnetckachev@yandex.ru>
2023-07-02 18:04:29 +0300

    Задача 23

---
+++ hw_23/hw_23.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System;
+
+int InputData()
+{
+    Write("Введите число ");
+    return(int.Parse(ReadLine()));
+}
+
+int Cube(int value)
+{
+    return value * value * value;
+}
+
+Clear();
+int number = InputData();
+for (int i = 1; i <= number; i++)
+{
+    Write($"{Cube(i)} ");
+}
