e006080078ed6c4325589c9f40f7a88bb71aaede
Netkachev <mnetckachev@yandex.ru>
2023-07-02 11:54:46 +0300

    Задача 2

---
+++ hw_2/hw_2.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else if (a < b)
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {a}");
+}
