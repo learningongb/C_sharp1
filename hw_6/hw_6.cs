f72eee8f87c9446cfff7ea868bd925738638c3db
Netkachev <mnetckachev@yandex.ru>
2023-07-02 11:55:29 +0300

    Задача 6

---
+++ hw_6/hw_6.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число. Проверим его четность. ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
