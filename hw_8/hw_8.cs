feb811f37ccf7c2a840b5e37b888a5b5b1370c2c
Netkachev <mnetckachev@yandex.ru>
2023-07-02 11:55:56 +0300

    Задача 8

---
+++ hw_8/hw_8.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+    i++;
+}
+
