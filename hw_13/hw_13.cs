c8d12bdc1275765cde850bcb0a7112e934e8112b
Netkachev <mnetckachev@yandex.ru>
2023-07-02 16:34:17 +0300

    Задача 13

---
+++ hw_13/hw_13.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System;
+
+Clear();
+Write("Введите число ");
+int a = int.Parse(ReadLine());
+
+while (a > 1000)
+{
+    a = a/10;
+}
+
+if (a < 100)
+{
+    WriteLine("Третьей цифры нет");
+}
+else
+{
+    WriteLine($"Третья цифра {a % 10}");
+}
