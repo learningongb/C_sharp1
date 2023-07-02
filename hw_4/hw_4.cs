57a28939b9107831579319c1115f7882e4e43bf8
Netkachev <mnetckachev@yandex.ru>
2023-07-02 11:55:05 +0300

    Задача 4

---
+++ hw_4/hw_4.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число 1 ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3 ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine(max);
