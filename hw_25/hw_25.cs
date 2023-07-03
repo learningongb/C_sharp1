2c8eb67906a3e50bcf2f5547dff159ba2b667589
Netkachev <mnetckachev@yandex.ru>
2023-07-03 22:29:43 +0300

    Задача 25

---
+++ hw_25/hw_25.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число ");
+int digit = int.Parse(ReadLine());
+Write("Введите степень ");
+int power = int.Parse(ReadLine());
+
+int result = 1;
+for (int i = 0; i < power; i++) 
+{
+    result *= digit;
+}
+
+WriteLine($"Результат возведения в степень {result}");
