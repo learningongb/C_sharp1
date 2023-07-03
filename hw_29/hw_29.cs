a2d9a33fa1796c461ddd5864b6662a0309c3c484
Netkachev <mnetckachev@yandex.ru>
2023-07-03 23:07:24 +0300

    Задача 29. Исправил создание Random в цикле

--- hw_29/hw_29.cs
+++ hw_29/hw_29.cs
@@ -1,8 +1,8 @@
 ﻿
 Console.Clear();
 int count = prompt("Введите число");
-int max = prompt("Введите максимальное число");
 int min = prompt("Введите минимальное число");
+int max = prompt("Введите максимальное число");
 printArray(getArray(count, min, max));
 
 void printArray(int[] array) 
@@ -17,9 +17,10 @@ void printArray(int[] array)
 int[] getArray(int count, int min, int max) 
 {
     int[] array = new int[count];
+    Random random = new Random();
     for (int i = 0; i < count; i++) 
     {
-        array[i] = new Random().Next(min, max + 1);
+        array[i] = random.Next(min, max + 1);
     }
     return array;
 }
