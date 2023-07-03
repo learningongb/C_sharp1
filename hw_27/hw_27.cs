9325fe0e10201857b8f12029e18eb6f6be737425
Netkachev <mnetckachev@yandex.ru>
2023-07-03 22:34:24 +0300

    Задача 27

---
+++ hw_27/hw_27.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число ");
+int digit = int.Parse(ReadLine());
+
+int result = 0;
+
+while (digit > 0) 
+{
+    result += digit % 10;
+    digit /= 10;
+}
+
+WriteLine($"Сумма цифр числа {result}");
