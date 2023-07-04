fa4b531c7d0a2adda1ee0888ced35b48c26470c4
Netkachev <mnetckachev@yandex.ru>
2023-07-04 22:45:21 +0300

    Задача 41

---
+++ hw_41/hw_41.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int count = Prompt("Введите количество чисел");
+int countPositive = 0;
+int number;
+for (int i = 0; i < count; i++)
+{
+    number = int.Parse(ReadLine());
+    if (number > 0)
+        countPositive++;
+}
+WriteLine($"Количество положительных = {countPositive}");
+
+int Prompt(String text)
+{
+    Write($"{text} ");
+    return int.Parse(ReadLine());
+}
