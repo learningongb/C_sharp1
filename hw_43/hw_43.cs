6294d718495729b52f2b6b1fb22d4d21e71a97d5
Netkachev <mnetckachev@yandex.ru>
2023-07-04 23:10:07 +0300

    Задача 43

---
+++ hw_43/hw_43.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+float b1 = Prompt("Введите b1");
+float k1 = Prompt("Введите k1");
+float b2 = Prompt("Введите b2");
+float k2 = Prompt("Введите k2");
+
+float x = (b2 - b1) / (k1 - k2);
+float y = k1 * x - b1;
+
+WriteLine(b2 - b1);
+WriteLine(k1 - k2);
+WriteLine((b2 - b1) / (k1 - k2));
+
+WriteLine($"Прямые пересекаются в точке Х={x}, Y={y}");
+
+float Prompt(String text)
+{
+    Write($"{text} ");
+    return float.Parse(ReadLine());
+}
