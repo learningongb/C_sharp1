26a34f2b957dc8147af8a17febb4312ba86f1f75
Netkachev <mnetckachev@yandex.ru>
2023-07-04 23:39:42 +0300

    Задача 47

---
+++ hw_47/hw_47.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+const int accuracy = 1;
+
+int countRows = Prompt("Введите количество строк");
+int countColumns = Prompt("Введите количество столбцов");
+
+printMatrix(generateMatrix(countRows, countColumns));
+
+void printMatrix(double[,] matrix)
+{
+    int maxi = matrix.GetUpperBound(0);
+    int maxj = matrix.GetUpperBound(1);
+    for (int i = 0; i <= maxi; i++)
+    {
+        for (int j = 0; j <= maxj; j++)
+        {
+            Write($"{Math.Round(matrix[i, j], accuracy)} ");
+        }
+        WriteLine();
+    }
+}
+
+double[,] generateMatrix(int countRows, int countColumns)
+{
+    double[,] matrix = new double[countRows, countColumns];
+    Random random = new Random();
+    for (int i = 0; i < countRows; i++)
+    {
+        for (int j = 0; j < countColumns; j++)
+        {
+            matrix[i, j] = random.NextDouble();
+        }
+    }
+    return matrix;
+}
+
+int Prompt(String text)
+{
+    Write($"{text} ");
+    return int.Parse(ReadLine());
+}
