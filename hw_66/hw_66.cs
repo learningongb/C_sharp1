efe37c4b95e1766594f7ae7afed713791c94096d
Netkachev <mnetckachev@yandex.ru>
2023-07-06 21:14:50 +0300

    Задача 66

---
+++ hw_66/hw_66.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int m = Prompt("Введите M");
+int n = Prompt("Введите N");
+WriteLine(SumNatural(m, n));
+
+int SumNatural(int min, int max)
+{
+    int sum = 0;
+    for (int i = 0; i < max - min + 1; i++)
+    {
+        sum += i + min;
+    }
+    return sum;
+}
+
+int Prompt(String text)
+{
+    Write($"{text} ");
+    return int.Parse(ReadLine());
+}
