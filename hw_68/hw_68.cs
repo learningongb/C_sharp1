4934dc309afa02945980d32c934edb08fbba090e
Netkachev <mnetckachev@yandex.ru>
2023-07-06 22:45:08 +0300

    Задача 68

---
+++ hw_68/hw_68.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int m = Prompt("Введите M");
+int n = Prompt("Введите N");
+
+if (Validate(m, n))
+{
+    WriteLine(AccermanFunction(m, n));
+}
+
+int AccermanFunction(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if (b == 0)
+    {
+        return AccermanFunction(a - 1, 1);
+    }
+    else
+    {
+        return AccermanFunction(a - 1, AccermanFunction(a, b - 1));
+    }
+}
+
+bool Validate(int m, int n)
+{
+    if (m > 3 || n > 5)
+    {
+        WriteLine("Не стоит задавать значения m > 3 и n > 5");
+        return false;
+    }
+    return true;
+}
+
+int Prompt(String text)
+{
+    Write($"{text} ");
+    return int.Parse(ReadLine());
+}
