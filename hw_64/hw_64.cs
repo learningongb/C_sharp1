90c3241298fe484c08eb3c52e4d89d55c97275e6
Netkachev <mnetckachev@yandex.ru>
2023-07-06 21:09:38 +0300

    Задача 64

---
+++ hw_64/hw_64.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int m = Prompt("Введите M");
+int n = Prompt("Введите N");
+int[] array = GetNatural(m, n);
+WriteLine(String.Join(", ", array));
+
+int[] GetNatural(int min, int max)
+{
+    int count = max - min + 1;
+    int[] result = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        result[i] = i + min;
+    }
+    return result;
+}
+
+int Prompt(String text)
+{
+    Write($"{text} ");
+    return int.Parse(ReadLine());
+}
